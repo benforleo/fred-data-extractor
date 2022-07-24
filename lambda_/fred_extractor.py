bbcc5f9b033476aba7ad7b1231998fa9b9749683
Ben Forleo <40501242+benforleo@users.noreply.github.com>
2022-07-24 15:51:12 -0400

    Working implementation

--- lambda_/fred_extractor.py
+++ lambda_/fred_extractor.py
@@ -1,28 +1,40 @@
 import logging
 from datetime import datetime, timedelta
+import json
 import requests
 import boto3
 
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 class FredExtractor:
     data = None
 
-    def __init__(self, event, context, session: boto3.Session) -> None:
+    def __init__(self, event, context, session: boto3.Session, bucket, series_id="SP500") -> None:
         self.event = event
         self.context = context
         self.session = session
+        self.bucket = bucket
+        self.series_id = series_id
+        self.observation_date = self.generate_observation_date(event)
 
-    def request_fred_data(self, series_id):
+    def execute(self):
+        self.request_fred_data() \
+            .store_fred_data_in_s3()
+
+    def request_fred_data(self):
+        logger.info("[FredExtractor][request_fred_data] has been called")
+
         url = "https://api.stlouisfed.org/fred/series/observations"
-        observation_date = self.generate_query_date(self.event)
-
+        observation_datestring = self.observation_date.strftime("%Y-%m-%d")
         params = {
-            'series_id': series_id,
+            'series_id': self.series_id,
             'frequency': 'd',
-            'observation_start': observation_date,
-            'observation_end': observation_date,
-            'api_key': self.retrieve_api_key()
+            'observation_start': observation_datestring,
+            'observation_end': observation_datestring,
+            'api_key': self.retrieve_api_key(),
+            'file_type': 'json'
         }
         try:
             r = requests.get(url, params=params)
@@ -32,32 +44,33 @@ class FredExtractor:
             raise err
         return self
 
-    def store_fred_data_in_s3(self, bucket):
+    def store_fred_data_in_s3(self):
         if len(self.data['observations']) == 0:
             logger.info("No data to save today")
         else:
             client = self.session.client("s3")
+            object_key = self.generate_s3_object_key()
+
             response = client.put_object(
-                Bucket=bucket,
-                Key="fred/SP500/year={}/month={}/SP500-{}.json",
-                Body=self.data
+                Bucket=self.bucket,
+                Key=object_key,
+                Body=json.dumps(self.data)
             )
 
     def retrieve_api_key(self):
-        client = self.session.client(
-            service_name='secretsmanager'
-        )
-        response = client.get_secret_value(
-            SecretId="dev/FredExtractor/APIKey"
-        )
-        return response['SecretString']['fred-api-key']
+        client = self.session.client(service_name='secretsmanager')
+        response = client.get_secret_value(SecretId="dev/FredExtractor/APIKey")
+        secret = json.loads(response['SecretString'])
+        return secret['fred-api-key']
+
+    def generate_s3_object_key(self):
+        year = str(self.observation_date.year)
+        month = str(self.observation_date.month).rjust(2, "0")
+        observation_datestring = self.observation_date.strftime("%Y-%m-%d")
+        return f"fred/{self.series_id}/year={year}/month={month}/{self.series_id}-{observation_datestring}.json"
 
     @staticmethod
-    def generate_query_date(event):
+    def generate_observation_date(event: dict) -> datetime:
         event_datetime = datetime.strptime(event['time'], "%Y-%m-%dT%H:%M:%SZ")
-        query_datetime = event_datetime - timedelta(days=1)
-        return query_datetime.strftime("%Y-%m-%d")
-
-
-
-
+        observation_datetime = event_datetime - timedelta(days=1)
+        return observation_datetime
--- lambda_/index.py
+++ lambda_/index.py
@@ -1,10 +1,22 @@
-import json
+import os
 import boto3
 from fred_extractor import FredExtractor
 
+bucket = os.getenv("FRED_BUCKET_NAME")
+
 
 def handler(event, context):
-    # session = boto3.session.Session()
-    # FredExtractor(session).retrieve_api_key()
+    session = boto3.session.Session()
 
-    return {'message': 'Hello world!'}
+    try:
+        fred = FredExtractor(
+            event,
+            context,
+            session,
+            bucket,
+            series_id="SP500"
+        )
+        fred.execute()
+
+    except Exception as err:
+        raise err
